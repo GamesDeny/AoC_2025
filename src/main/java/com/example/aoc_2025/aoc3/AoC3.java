fc7761a5845b4498c610abc630d3e195f4f80a70
Francesco Pio <67315340+GamesDeny@users.noreply.github.com>
2025-12-05 22:26:26 +0100

    aoc3p1 solved

---
+++ src/main/java/com/example/aoc_2025/aoc3/AoC3.java
@@ -0,0 +1,89 @@
+package com.example.aoc_2025.aoc3;
+
+import com.example.aoc_2025.AbstractAoC;
+import com.google.common.base.Splitter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+The batteries are arranged into banks; each line of digits in your input corresponds to a single bank of batteries. Within each bank, you need to turn on exactly two batteries;
+ he joltage that the bank produces is equal to the number formed by the digits on the batteries you've turned on.
+
+ For example, if you have a bank like 12345 and you turn on batteries 2 and 4, the bank would produce 24 jolts. (You cannot rearrange batteries.)
+You'll need to find the largest possible joltage each bank can produce. In the above example:
+
+In 987654321111111, you can make the largest joltage possible, 98, by turning on the first two batteries.
+In 811111111111119, you can make the largest joltage possible by turning on the batteries labeled 8 and 9, producing 89 jolts.
+In 234234234234278, you can make 78 by turning on the last two batteries (marked 7 and 8).
+In 818181911112111, the largest joltage you can produce is 92.
+The total output joltage is the sum of the maximum joltage from each bank, so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
+* */
+public class AoC3 extends AbstractAoC<Long> {
+
+    public static void main(String[] args) {
+        new AoC3().execute();//res =17427
+    }
+
+    @Override
+    public Long exec(File file) {
+        var result = 0L;
+        try (var fis = new FileInputStream(file); var sc = new Scanner(fis)) {
+            while (sc.hasNextLine()) {
+                var bank = Splitter.fixedLength(getSplitSize())
+                        .splitToList(sc.nextLine()).stream()
+                        .map(Integer::parseInt)
+                        .toList();
+                var maxL = bank.stream()
+                        .max(Integer::compareTo)
+                        .orElseThrow(() -> new IllegalArgumentException("Bank is empty"));
+
+                var lIndex = 0;
+                for (var i = 0; i < bank.size(); i++) {
+                    if (bank.get(i).equals(maxL)) {
+                        lIndex = i;
+                        break;
+                    }
+                }
+                var otherMax = 0;
+                if (lIndex == bank.size() - 1) {
+                    otherMax = bank.stream()
+                            .limit(lIndex)
+                            .max(Integer::compareTo)
+                            .orElseThrow(() -> new IllegalArgumentException("Bank is empty"));
+                    result += otherMax * 10L + maxL;
+                } else {
+                    otherMax = bank.stream()
+                            .skip(lIndex + 1L)
+                            .max(Integer::compareTo)
+                            .orElseThrow(() -> new IllegalArgumentException("Bank is empty"));
+                    result += maxL * 10L + otherMax;
+                }
+            }
+        } catch (FileNotFoundException _) {
+            logger.severe("File not found");
+        } catch (IOException _) {
+            logger.severe("Error in file read");
+        }
+
+        return result;
+    }
+
+    int getSplitSize() {
+        return 1;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Long expectedValue() {
+        return 357L;
+    }
+
+}
